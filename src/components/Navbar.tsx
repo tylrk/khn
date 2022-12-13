8e0c54bd5f7ecd6ab389cab6a9d5580c3d4f9785
tylrk <tylerk1117@gmail.com>
2022-12-12 18:49:00 -0600

    formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,36 @@
 import React from "react";
-import '../App.css';
+import "../App.css";
 import { DarkModeToggle, Mode } from "@anatoliygatt/dark-mode-toggle";
 
 interface NavbarProps {
-    theme: Mode;
-    changeTheme: () => void;
+  theme: Mode;
+  changeTheme: () => void;
 }
 
 const Navbar: React.FC<NavbarProps> = (props) => {
-    return (
-        <nav className="navbar">
-          <h1 className="title">KHN</h1>
-          <div className="toggle">
-          <DarkModeToggle
-            mode={props.theme}
-            dark="Dark"
-            light="Light"
-            size="sm"
-            inactiveTrackColor="#e2e8f0"
-            inactiveTrackColorOnHover="#f8fafc"
-            inactiveTrackColorOnActive="#cbd5e1"
-            activeTrackColor="#334155"
-            activeTrackColorOnHover="#1e293b"
-            activeTrackColorOnActive="#0f172a"
-            inactiveThumbColor="#1e293b"
-            activeThumbColor="#e2e8f0"
-            onChange={props.changeTheme}
-            ariaLabel="Toggle color scheme"
-          />
-          </div>
+  return (
+    <nav className="navbar">
+      <h1 className="title">KHN</h1>
+      <div className="toggle">
+        <DarkModeToggle
+          mode={props.theme}
+          dark="Dark"
+          light="Light"
+          size="sm"
+          inactiveTrackColor="#e2e8f0"
+          inactiveTrackColorOnHover="#f8fafc"
+          inactiveTrackColorOnActive="#cbd5e1"
+          activeTrackColor="#334155"
+          activeTrackColorOnHover="#1e293b"
+          activeTrackColorOnActive="#0f172a"
+          inactiveThumbColor="#1e293b"
+          activeThumbColor="#e2e8f0"
+          onChange={props.changeTheme}
+          ariaLabel="Toggle color scheme"
+        />
+      </div>
     </nav>
-    )
+  );
 };
 
 export default Navbar;
